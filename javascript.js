d12044cb5368e00e4845f109bb272195687769db
oscar <222574796+raxzy0@users.noreply.github.com>
2025-12-26 18:46:45 +1100

    basic features complete

--- javascript.js
+++ javascript.js
@@ -1,11 +1,42 @@
 
 let container = document.querySelector(".container")
+let btn = document.querySelector("button")
+let num = 0
 
 
-for (let i = 0; i < 16; i++) {
+btn.addEventListener('click', () => {
+    deletegrid()
+    num = prompt("How many squares?:")
+    
+    for (let i = 0; i < num; i++) {
     let row = document.createElement("div")
-    row.classList.add('row')
-    document.querySelector('.row')
-    row.textContent = "p"
+    row.classList.add('row' + i, 'row')
+    document.querySelector('row')
     container.append(row)
+
+    for (let i = 0; i < num; i++) {
+        let col = document.createElement("div")
+        col.classList.add('col' + i, 'square')
+        document.querySelector('.square')
+        row.append(col)
+    }
+    const boxes = document.querySelectorAll('.square')
+    boxes.forEach(box => {
+        box.style.height = (700/num) + 'px'
+        box.style.width = (700/num ) + 'px'
+    })
+}
+
+container.addEventListener('mouseover', (e) => {
+    e.target.addEventListener('mouseenter', () => {
+        e.target.style.backgroundColor = 'blue'
+    })
+})
+})
+
+function deletegrid() {
+    for (let i = 0; i < num; i++) {
+    let row = document.querySelector('.row')
+    container.removeChild(row)
+    }   
 }
